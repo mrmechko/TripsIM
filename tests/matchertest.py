140906e58a67da1f22f70ddf63922d6afb7ad30f
Jason Brar <jason.s.brar@gmail.com>
2019-05-02 13:40:00 -0400

    Update README

---
+++ tests/matchertest.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.insert(0, os.path.abspath('../..'))
+import IM.PyIM as PyIM
+from tripsweb.query import wsd as parse
+from IM.PyIM import matcher
+
+if __name__ == '__main__':
+    # Needs to be filled in
+    assert True
